8c8c5ff832dfd5537c5f4e1d480b9570f07d9ecb
Jomy10 <jonas.vbs4@gmail.com>
2023-05-06 19:51:51 +0200

    initial commit

---
+++ api/index.rb
@@ -0,0 +1,7 @@
+Handler = Proc.new do |req, resp|
+  name = req.query['name'] || "World"
+
+  resp.status = 200
+  resp['Content-Type'] = 'text/text; charset=utf8'
+  resp.body = "Hello #{name}!"
+end
